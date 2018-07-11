1ec5a46c11ab3dc77eb1931b4894bfcc868aa50b
xusanpangzi <767306336@qq.com>
2018-07-11 16:20:37 +0800

    Initial commit

---
+++ myblog/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class BlogsPost(models.Model):
+    title=models.CharField(max_length=150)
+    body=models.TextField()
+    timestamp=models.DateField()
